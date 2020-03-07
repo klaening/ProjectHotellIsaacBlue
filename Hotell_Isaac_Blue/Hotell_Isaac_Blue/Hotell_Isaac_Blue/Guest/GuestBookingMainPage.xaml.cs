8274d222808e125a1fafff4b62e3db92f3fe17a3
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-07 12:11:51 +0100

    First commit to BookingFirstPage

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestBookingMainPage.xaml.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestBookingMainPage.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace Hotell_Isaac_Blue
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class GuestBookingPage : ContentPage
+    public partial class GuestBookingMainPage : ContentPage
     {
-        public GuestBookingPage()
+        public GuestBookingMainPage()
         {
             InitializeComponent();
         }
