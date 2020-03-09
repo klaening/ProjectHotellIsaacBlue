6e080568e1032c1e1ae79ea844584e022cb7808f
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-09 15:27:06 +0100

    Started with design of the booking result page

---
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestBookingThirdPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hotell_Isaac_Blue
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GuestBookingThirdPage : ContentPage
+    {
+        public GuestBookingThirdPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
