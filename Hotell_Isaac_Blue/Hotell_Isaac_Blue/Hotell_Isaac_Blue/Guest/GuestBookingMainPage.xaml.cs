13501500bdd3a8c61f34c735285bb5e5a4fe9a05
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-09 11:28:03 +0100

    Added botton to GuestBookingSecondPage

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestBookingMainPage.xaml.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestBookingMainPage.xaml.cs
@@ -16,5 +16,10 @@ namespace Hotell_Isaac_Blue
         {
             InitializeComponent();
         }
+
+        private void NewBooking_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GuestBookingSecondPage());
+        }
     }
 }
