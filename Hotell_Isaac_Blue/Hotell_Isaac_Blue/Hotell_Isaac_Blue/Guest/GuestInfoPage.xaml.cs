da5a932013b579e97c3deac4b73b0a76c6016c90
AlexanderLeafet <alexander.lovdahl@live.se>
2020-03-10 15:01:00 +0100

    Created GuesInfoPage and inserted google maps via API

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestInfoPage.xaml.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestInfoPage.xaml.cs
@@ -6,15 +6,52 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.GoogleMaps;
 
 namespace Hotell_Isaac_Blue
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GuestInfoPage : ContentPage
     {
+        private void AddMarkers()
+        {
+            Map map = new Map
+            {
+
+            };
+
+            var mapPosition = new Position(57.701629, 11.913693);
+
+
+            Pin pin = new Pin
+            {
+                IsVisible = true,
+                Label = "Hotel Isaac",
+                Address = "Maskingatan 5, Gothenburg",
+                Type = PinType.Place,
+                Position = mapPosition
+
+            };
+
+            map.Pins.Add(pin);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(mapPosition, Distance.FromKilometers(30)));
+        }
+
         public GuestInfoPage()
         {
             InitializeComponent();
+            AddMarkers();
+
+        }
+
+        private void Map_MapClicked(object sender, MapClickedEventArgs e)
+        {
+       
+        }
+
+        private void Map_PinClicked(object sender, PinClickedEventArgs e)
+        {
+
         }
     }
 }
