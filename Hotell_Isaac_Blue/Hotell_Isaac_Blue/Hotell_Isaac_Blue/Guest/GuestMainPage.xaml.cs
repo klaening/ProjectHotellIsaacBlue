b5c3944e2a4f0c3176d85f809f5c8e936c8abb2e
Mikael <m.klaening@gmail.com>
2020-03-13 14:02:03 +0100

    Design v2

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestMainPage.xaml.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestMainPage.xaml.cs
@@ -15,6 +15,46 @@ namespace Hotell_Isaac_Blue
         public GuestMainPage()
         {
             InitializeComponent();
+            AddTapGestures();
+        }
+
+        private void AddTapGestures()
+        {
+            var tapBookingsFrame = new TapGestureRecognizer();
+            tapBookingsFrame.Tapped += BookingsFrame_Tapped;
+            BookingsFrame.GestureRecognizers.Add(tapBookingsFrame);
+
+            var tapInfoFrame = new TapGestureRecognizer();
+            tapInfoFrame.Tapped += InfoFrame_Tapped;
+            InfoFrame.GestureRecognizers.Add(tapInfoFrame);
+
+            var tapBreakfastFrame = new TapGestureRecognizer();
+            tapBreakfastFrame.Tapped += BreakfastFrame_Tapped;
+            BreakfastFrame.GestureRecognizers.Add(tapBreakfastFrame);
+
+            var tapParkingFrame = new TapGestureRecognizer();
+            tapParkingFrame.Tapped += ParkingFrame_Tapped;
+            ParkingFrame.GestureRecognizers.Add(tapParkingFrame);
+        }
+
+        private void ParkingFrame_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GuestParkingPage());
+        }
+
+        private void BreakfastFrame_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GuestBreakfastPage());
+        }
+
+        private void InfoFrame_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GuestInfoPage());
+        }
+
+        private void BookingsFrame_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GuestBookingMainPage());
         }
 
         private void BookingBtn_Clicked(object sender, EventArgs e)
