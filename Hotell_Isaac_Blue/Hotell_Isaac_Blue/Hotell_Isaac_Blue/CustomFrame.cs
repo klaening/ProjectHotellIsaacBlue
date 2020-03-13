1faa7e3bf9a5b913e88857fc61670508f72a4f1f
Mikael <m.klaening@gmail.com>
2020-03-13 15:37:16 +0100

    Changed standard values for Frame and Button

---
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/CustomFrame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Hotell_Isaac_Blue
+{
+    public class CustomFrame : Frame
+    {
+        StackLayout parent = null;
+
+        public CustomFrame()
+        {
+            parent = new StackLayout();
+
+            Grid grid = new Grid()
+            {
+                
+            };
+
+            Frame frame = new Frame
+            {
+
+            };
+
+
+        }
+    }
+}
