b923a073fba35fd842d68e214f7d2964e2b7c5e0
Mikael <m.klaening@gmail.com>
2020-03-23 17:56:47 +0100

    Added functionality to DatePickers

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestBooking/GuestBookingSecondPage.xaml.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestBooking/GuestBookingSecondPage.xaml.cs
@@ -13,6 +13,10 @@ namespace Hotell_Isaac_Blue
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GuestBookingSecondPage : ContentPage
     {
+        DateTime dateMinValue = DateTime.MinValue;
+        DateTime startDate;
+        DateTime endDate;
+
         ScrollView parent = null;
         StackLayout stack1 = null;
         static StackLayout stack2 = null;
@@ -88,6 +92,90 @@ namespace Hotell_Isaac_Blue
             //Content = parent;
         }
 
+        private void SDFrame_Tapped(object sender, EventArgs e)
+        {
+            DatePickerSD.Focus();
+        }
+
+        private void EDFrame_Tapped(object sender, EventArgs e)
+        {
+            DatePickerED.Focus();
+        }
+
+        private void DatePickerSD_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DateTime pickedDate = e.NewDate;
+
+            if (pickedDate < DateTime.Now.Date)
+            {
+                DisplayAlert("Alert", "Start date can not precede today's date", "OK");
+            }
+            else if (endDate != dateMinValue && pickedDate >= endDate)
+            {
+                DisplayAlert("Alert", "End date can not precede start date", "OK");
+            }
+            else
+            {
+                startDate = e.NewDate;
+                (string date, string year) = ReturnDateAndYear(e.NewDate.ToLongDateString());
+
+                SDDateLabel.Text = date;
+                SDYearLabel.Text = year;
+
+                if (EDDateLabel.Text != null)
+                    TotDaysLabel.Text = CalculateTotalDays();
+            }
+        }
+
+        private void DatePickerED_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DateTime pickedDate = e.NewDate;
+
+            if (pickedDate <= DateTime.Now.Date)
+            {
+                DisplayAlert("Alert", "End date can not precede today's date", "OK");
+            }
+            else if (pickedDate <= startDate)
+            {
+                DisplayAlert("Alert", "End date can not precede start date", "OK");
+            }
+            else
+            {
+                endDate = e.NewDate;
+                (string date, string year) = ReturnDateAndYear(e.NewDate.ToLongDateString());
+
+                EDDateLabel.Text = date;
+                EDYearLabel.Text = year;
+
+                if (SDDateLabel.Text != null)
+                    TotDaysLabel.Text = CalculateTotalDays();
+            }
+        }
+
+        private (string date, string year) ReturnDateAndYear(string longDate)
+        {
+            string[] dateSplit = longDate.Split(' ');
+            string date = dateSplit[0].Remove(3) + ",";
+            dateSplit[1] = dateSplit[1].Remove(3);
+            dateSplit[2] = dateSplit[2].Remove(dateSplit[2].Length - 1);
+            string year = dateSplit[3];
+
+            for (int i = 1; i < 3; i++)
+            {
+                date += " " + dateSplit[i];
+            }
+
+            return (date, year);
+        }
+
+        private string CalculateTotalDays()
+        {
+            TimeSpan ts = endDate - startDate;
+            int days = (int)ts.TotalDays;
+
+            return "Total days: " + days;
+        }
+
         private void Add_Clicked(object sender, EventArgs e)
         {
             RoomNo++;
@@ -142,34 +230,23 @@ namespace Hotell_Isaac_Blue
             }
         }
 
-        private void SDFrame_Tapped(object sender, EventArgs e)
-        {
-            DatePickerSD.Focus();
-        }
-
-        private void EDFrame_Tapped(object sender, EventArgs e)
-        {
-            DatePickerED.Focus();
-        }
-
-        private void DatePickerSD_DateSelected(object sender, EventArgs e)
-        {
-
-        }
-
-        private void DatePickerED_DateSelected(object sender, EventArgs e)
-        {
-
-        }
-
         private void Options_Btn_Clicked(object sender, EventArgs e)
         {
-
+            if (OptionsFrame.IsVisible == false)
+            {
+                Options_Btn.Text = "-";
+                OptionsFrame.IsVisible = true;
+            }
+            else
+            {
+                Options_Btn.Text = "+";
+                OptionsFrame.IsVisible = false;
+            }
         }
 
         private void Result_Btn_Clicked(object sender, EventArgs e)
         {
-
+            Navigation.PushAsync(new GuestBookingThirdPage());
         }
     }
 }
