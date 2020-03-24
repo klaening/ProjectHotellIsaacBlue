a4dde7776ade6e27b92adcd249d7f9cdbe3c782c
Mikael <m.klaening@gmail.com>
2020-03-24 15:38:00 +0100

    Tried adding Azure commands to project

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Models/CustomerTypes.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Models/CustomerTypes.cs
@@ -4,7 +4,10 @@ using System.Text;
 
 namespace Hotell_Isaac_Blue.Models
 {
-    class CustomerTypes
+    public class CustomerTypes
     {
+        public int ID { get; set; }
+        public string TYPENAME { get; set; }
+        public int DISCOUNT { get; set; }
     }
 }
--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Models/Customers.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Models/Customers.cs
@@ -1,10 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace Hotell_Isaac_Blue.Models
 {
-    class Customers
+    public class Customers
     {
+        public int ID { get; set; }
+        public string SOCNUMBER { get; set; }
+        public string FIRSTNAME { get; set; }
+        public string LASTNAME { get; set; }
+        public string EMAIL { get; set; }
+        public string STREETADDRESS { get; set; }
+        public string CITY { get; set; }
+        public string COUNTRY { get; set; }
+        public string ICE { get; set; }
+        public int CUSTOMERTYPESID { get; set; }
+
+        public static MobileServiceClient client = new MobileServiceClient("https://hotellisaacbluewebapi.azurewebsites.net");
+
+        public async Task<bool> SaveCustomer()
+        {
+            try
+            {
+                await client.GetTable<Customers>().InsertAsync(this);
+                return true;
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                var response = await msioe.Response.Content.ReadAsStringAsync();
+                return false;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public static async Task<List<Customers>> ReadCustomers()
+        {
+            return await client.GetTable<Customers>().ToListAsync();
+        } 
+
+        public async void HaconBacon()
+        {
+            var list = client.GetTable<Customers>().ToListAsync();
+        }
     }
 }
