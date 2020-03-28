c12a2866b10b0b0b7e96fb45db548f5c897d77cf
Mikael <m.klaening@gmail.com>
2020-03-28 12:02:04 +0100

    First try

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestBookingSecondPage.xaml.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Guest/GuestBookingSecondPage.xaml.cs
@@ -14,138 +14,52 @@ namespace Hotell_Isaac_Blue
 
     public partial class GuestBookingSecondPage : ContentPage
     {
-        ScrollView parent = null;
-        StackLayout stack1 = null;
-        static StackLayout stack2 = null;
-        StackLayout stack3 = null;
-        StackLayout stackParent = null;
         static int RoomNo;
         static List<CustomFrame> frameList = new List<CustomFrame>();
 
         public GuestBookingSecondPage()
         {
-            //InitializeComponent();
-
-            RoomNo = 1;
-
-            BackgroundColor = Color.LightBlue;
-
-            parent = new ScrollView();
-
-            stack1 = new StackLayout
-            {
-                Padding = 0,
-                Margin = 0,
-
-                Children =
-                {
-                    new CustomFrame(RoomNo)
-                }
-            };
-
-            stack2 = new StackLayout
-            { 
-                Padding = 0,
-                Margin = 0
-            };
-
-            stack3 = new StackLayout
-            {
-                Padding = 0,
-                Margin = 0
-            };
-
-            Button add = new Button
-            {
-                Text = "+",
-                TextColor = Color.Black,
-                FontSize = 30,
-                HorizontalOptions = LayoutOptions.Center,
-                BackgroundColor = Color.Aqua,
-                BorderColor = Color.SlateGray,
-                BorderWidth = 5,
-                CornerRadius = 10,
-                HeightRequest = 100,
-                WidthRequest = 100,
-                Opacity = 0.2
-            };
-
-            add.Clicked += Add_Clicked;
-
-            stack3.Children.Add(add);
-
-            stackParent = new StackLayout
-            {
-                Children =
-                {
-                    stack1,
-                    stack2,
-                    stack3
-                }
-            };
-
-            parent.Content = stackParent;
-
-            Content = parent;
-        }
-
-        private void Add_Clicked(object sender, EventArgs e)
-        {
-            RoomNo++;
-
-            var frame = new CustomFrame(RoomNo);
-            frameList.Add(frame);
-
-            stack2.Children.Add(frame);
+            InitializeComponent();
         }
 
         private void OptionsBtn_Clicked(object sender, EventArgs e)
         {
             //More options
         }
-
-        public static void RemoveFromStack(int roomNo)
+        private void AddBtn_Clicked(object sender, EventArgs e)
         {
-            int frameIndex = roomNo - 2;
+            RoomNo++;
 
-            if (roomNo > 1)
-            {
-                stack2.Children.Remove(frameList[frameIndex]);
-                frameList.Remove(frameList[frameIndex]);
-                RoomNo--;
+            var frame = new CustomFrame(RoomNo);
+            frameList.Add(frame);
 
-                //Ändra nummer på alla rum med ett rumsnummer högre än det som togs bort
-                ChangeRoomNumbers(roomNo);
-            }
+            FrameStack.Children.Add(frame);
         }
 
-        private static void ChangeRoomNumbers(int roomNo)
+        public void RemoveFromStack(int roomNo)
         {
-            //Leta efter alla rum som har nummer högre än roomNo
-            List<CustomFrame> frameChildren = new List<CustomFrame>();
-
-            foreach (var child in stack2.Children)
+            CustomFrame actualFrame = null;
+            //Hitta frame i listan
+            foreach (var frame in frameList)
             {
-                if (child is CustomFrame)
+                if (frame.RoomNo == roomNo)
                 {
-                    frameChildren.Add((CustomFrame)child);
+                    actualFrame = frame;
+                    break;
                 }
             }
 
-            //Kommer kunna föra in detta i förra loopen
-            foreach (var frame in frameChildren)
-            {
-                if (frame.RoomNo > roomNo)
-                {
-                    frame.RoomNo -= 1;
-                    frame.textLabel.Text = "Room " + frame.RoomNo;
-                }
-            }
+            FrameStack.Children.Remove(actualFrame);
         }
 
         private void SDFrame_Tapped(object sender, EventArgs e)
         {
             SDPicker.IsVisible = true;
+        }
+
+        private void EDFrameTapped(object sender, EventArgs e)
+        {
+            EDPicker.IsVisible = true;
         }
     }
 }
