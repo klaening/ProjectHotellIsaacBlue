4d01297421bcac52886de37bcab15434a5de2ff1
Mattis <Mattis.vainikainen@gmail.com>
2020-04-01 11:17:45 +0200

    update

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/GuestPages/BreakFast/Views/BreakfastFirstPage.xaml.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/GuestPages/BreakFast/Views/BreakfastFirstPage.xaml.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Hotell_Isaac_Blue.GuestPages.BreakFast.Models;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Net.Http;
+using Hotell_Isaac_Blue.Tables;
+using System.Diagnostics;
 
 namespace Hotell_Isaac_Blue.GuestPages.BreakFast.Views
 {
@@ -19,23 +22,28 @@ namespace Hotell_Isaac_Blue.GuestPages.BreakFast.Views
             InitializeComponent();           
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            //string path = "bookings/";
+            string path = "bookings/";
 
-            //string[] key = new string[] { BookingEntry.Text };
+            string[] key = new string[] { BookingEntry.Text };
 
-            //var response = APIServices.Services.
-
-            BreakfastModel.BookingNo = BookingEntry.Text;
-            var client = new HttpClient();
-            var response = client.GetAsync("https://hotellisaacbluewebapi.azurewebsites.net/api/bookings/" + BreakfastModel.BookingNo);
-
-            string jsonData = string.Empty;
-
-            if(response.Result.IsSuccessStatusCode)
+            var response = APIServices.Services.GetService(path, key);
+            string result = await response.Content.ReadAsStringAsync();
+            var testBooking = JsonConvert.DeserializeObject<Bookings>(result);
+            
+            if(response.IsSuccessStatusCode)
             {
+                await DisplayAlert("Success!", "Added Breakfast", "ok");
+                ActiveBooking.Booking = testBooking;
+                ActiveBooking.Booking.BREAKFAST = false;
+                //Update webapi!
                 
+                
+            }
+            else
+            {
+                await DisplayAlert("Error!", "Deleted Request", "ok");
             }
            
         }
