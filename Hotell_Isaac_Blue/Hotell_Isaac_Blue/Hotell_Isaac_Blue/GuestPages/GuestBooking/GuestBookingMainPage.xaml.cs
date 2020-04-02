5b9280cf85fb5ec8988c32cf7fcd044dd84c1f3e
AlexanderLeafet <alexander.lovdahl@live.se>
2020-04-02 14:57:36 +0200

    Added functionality to get made booking shown

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/GuestPages/GuestBooking/GuestBookingMainPage.xaml.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/GuestPages/GuestBooking/GuestBookingMainPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Hotell_Isaac_Blue.APIServices;
+using Hotell_Isaac_Blue.Tables;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +17,36 @@ namespace Hotell_Isaac_Blue
         public GuestBookingMainPage()
         {
             InitializeComponent();
+            GetBookings();
+            if(ActiveBooking.Booking.ID != null)
+            {
+                BookingDetails();
+            }
+            
+        }
+
+        
+
+        private async void GetBookings()
+        {
+            var path = "bookings/";
+
+            string[] source = new string[] { Convert.ToString(ActiveUser.Account.CustomersID) };
+
+            var response = Services.GetService(path, source);
+            string result = await response.Content.ReadAsStringAsync();
+
+            var activeBooking = JsonConvert.DeserializeObject<Bookings>(result);
+
+            ActiveBooking.Booking = activeBooking;
+        }
+
+        
+        public void BookingDetails()
+        {
+            bookingNr.Text = Convert.ToString(ActiveBooking.Booking.ID);
+            startDate.Text = Convert.ToString(ActiveBooking.Booking.STARTDATE);
+            endDate.Text = Convert.ToString(ActiveBooking.Booking.ENDDATE);
         }
 
         private void NewBooking_Clicked(object sender, EventArgs e)
