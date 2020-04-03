dcf7f01165fd1a92f2be3111a6765aba77a676f5
AndyCarlsson <andreas95carlsson@gmail.com>
2020-04-03 15:28:39 +0200

    Created bransch and set up xaml for a settings page

---
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/SettingsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hotell_Isaac_Blue
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetCustomer()
+        {
+            //En metod som hämtar den inloggade kunden och fyller i dess info om det finns.
+        }
+
+        private void UpdateInfo_Clicked(object sender, EventArgs e)
+        {
+            //En metod som updaterar den inloggade kundens uppgifter.
+        }
+    }
+}
