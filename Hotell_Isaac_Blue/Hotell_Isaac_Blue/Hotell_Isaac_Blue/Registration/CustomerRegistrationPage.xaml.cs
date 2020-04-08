b8034ebc5504cab421f36165ee99f5e6cbc777af
AndyCarlsson <andreas95carlsson@gmail.com>
2020-04-08 12:01:40 +0200

    Moved logic from settingspage to customerRegistrationPage

--- Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Registration/CustomerRegistrationPage.xaml.cs
+++ Hotell_Isaac_Blue/Hotell_Isaac_Blue/Hotell_Isaac_Blue/Registration/CustomerRegistrationPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Hotell_Isaac_Blue.Tables;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,34 +14,82 @@ namespace Hotell_Isaac_Blue
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CustomerRegistrationPage : ContentPage
     {
+        Customers customer;
+
         public CustomerRegistrationPage()
         {
             InitializeComponent();
+            if (ActiveUser.Account.CustomersID != null)
+                RegisterBtn.Text = "Update information";
+            SetCustomerInfo();
         }
+        private async void SetCustomerInfo()
+        {
+            //Kolla över denna lösning på if. HasValue kan medföra problem
+            if (ActiveUser.Account.CustomersID != null)
+            {
+                string path = "customers/";
+                string source = ActiveUser.Account.CustomersID.ToString();
 
+                var response = APIServices.Services.GetRequest(path, source);
+                var result = await response.Content.ReadAsStringAsync();
+
+                customer = JsonConvert.DeserializeObject<Customers>(result);
+
+                socNrEntry.Text = customer.SOCNUMBER;
+                firstNameEntry.Text = customer.FIRSTNAME;
+                lastNameEntry.Text = customer.LASTNAME;
+                emailEntry.Text = customer.EMAIL;
+                cityEntry.Text = customer.CITY;
+                countryEntry.Text = customer.COUNTRY;
+                streetAdressEntry.Text = customer.STREETADRESS;
+                iceEntry.Text = customer.ICE;
+            }
+        }
         private async void RegisterBtn_Clicked(object sender, EventArgs e)
         {
-
-            // Skapa en get på Customer socnumber i web api
-
-            Customers customer = new Customers()
+            if (ActiveUser.Account.CustomersID != null)
             {
-                SOCNUMBER = socNrEntry.Text,
-                FIRSTNAME = firstNameEntry.Text,
-                LASTNAME = lastNameEntry.Text,
-                EMAIL = emailEntry.Text,
-                STREETADRESS = streetAdressEntry.Text,
-                CITY = cityEntry.Text,
-                COUNTRY = countryEntry.Text,
-                ICE = iceEntry.Text,
-            };
+                string path = "customers/";
 
-            //Default är 1
-            customer.CUSTOMERTYPESID = 1;
+                customer.SOCNUMBER = socNrEntry.Text;
+                customer.FIRSTNAME = firstNameEntry.Text;
+                customer.LASTNAME = lastNameEntry.Text;
+                customer.EMAIL = emailEntry.Text;
+                customer.CITY = cityEntry.Text;
+                customer.COUNTRY = countryEntry.Text;
+                customer.STREETADRESS = streetAdressEntry.Text;
+                customer.ICE = iceEntry.Text;
 
-            string path = "customers/account/" + ActiveUser.Account.ID;
+                await APIServices.Services.PutRequestAsync(path, customer);
 
-            await APIServices.Services.PostRequestAsync(path, customer);
+                await DisplayAlert("Updated!", "Your information have been updated.", "Ok");                
+            }
+            else if (ActiveUser.Account.CustomersID == null)
+            {
+                Customers customer = new Customers()
+                {
+                    SOCNUMBER = socNrEntry.Text,
+                    FIRSTNAME = firstNameEntry.Text,
+                    LASTNAME = lastNameEntry.Text,
+                    EMAIL = emailEntry.Text,
+                    STREETADRESS = streetAdressEntry.Text,
+                    CITY = cityEntry.Text,
+                    COUNTRY = countryEntry.Text,
+                    ICE = iceEntry.Text,
+                };
+
+                //Default är 1
+                customer.CUSTOMERTYPESID = 1;
+
+                string path = "customers/account/" + ActiveUser.Account.ID;
+
+                await APIServices.Services.PostRequestAsync(path, customer);
+
+                await DisplayAlert("Saved!", "Your information have been saved.", "Ok");
+            }
+
+            await Navigation.PopAsync();
         }
     }
 }
